d0ce7fe311024ec9f8c817ab9552373baa1638df
Lucas Marques <cmarques.lucas@gmail.com>
2017-05-21 11:46:43 -0300

    storing and fetching shopping list

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {IonicPage, PopoverController} from 'ionic-angular';
+import {IonicPage, PopoverController, LoadingController, AlertController} from 'ionic-angular';
 import { ShoppingListService } from "../../services/shopping-list";
 import {NgForm} from "@angular/forms";
 import {Ingredient} from "../../models/ingredient";
 import {SLOptionsPage} from "./sl-options/sl-options";
+import {AuthService} from "../../services/auth";
 
 @IonicPage()
 @Component({
@@ -13,7 +14,11 @@ import {SLOptionsPage} from "./sl-options/sl-options";
 export class ShoppingListPage {
   listItems: Ingredient[];
 
-  constructor(private shoppingListService: ShoppingListService, private popoverController: PopoverController) {
+  constructor(private shoppingListService: ShoppingListService,
+              private popoverController: PopoverController,
+              private authService: AuthService,
+              private loadingController: LoadingController,
+              private alertController: AlertController) {
   }
 
   ionViewWillEnter() {
@@ -37,8 +42,51 @@ export class ShoppingListPage {
   }
 
   onShowOptions(event: MouseEvent) {
+    const loading = this.loadingController.create({
+      content: 'Please wait...'
+    });
     const popover = this.popoverController.create(SLOptionsPage);
     popover.present({ev: event});
+    popover.onDidDismiss(data => {
+      if (data.action == 'load') {
+        loading.present();
+        this.authService.getActiveUser().getToken()
+          .then((token: string) => {
+            this.shoppingListService.fetchList(token)
+              .subscribe((list: Ingredient[]) => {
+                loading.dismiss();
+                if (list) {
+                  this.listItems = list;
+                } else {
+                  this.listItems = [];
+                }
+              }, error => {
+                loading.dismiss();
+                this.handleError(error);
+              })
+          });
+      } else if (data.action == 'store') {
+        loading.present();
+        this.authService.getActiveUser().getToken()
+          .then((token: string) => {
+            this.shoppingListService.storeList(token)
+              .subscribe(() => {
+                loading.dismiss()
+              }, error => {
+                loading.dismiss();
+                this.handleError(error);
+              })
+          });
+      }
+    });
+  }
+
+  handleError(errorMessage: string) {
+    this.alertController.create({
+      title: 'An error ocurred!',
+      message: errorMessage,
+      buttons: ['OK']
+    }).present();
   }
 
 }
--- src/services/shopping-list.ts
+++ src/services/shopping-list.ts
@@ -1,7 +1,15 @@
 import { Ingredient } from "../models/ingredient";
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {AuthService} from "./auth";
+import  'rxjs/Rx';
 
+@Injectable()
 export class ShoppingListService {
+
   private ingredients: Ingredient[] = [];
+
+  constructor(private http: Http, private authService: AuthService) { }
 
   addItem(name: string, amount: number) {
     this.ingredients.push(new Ingredient(name, amount));
@@ -19,4 +27,23 @@ export class ShoppingListService {
   removeItem(index: number) {
     this.ingredients.splice(index, 1);
   }
+
+  storeList(token: string) {
+    const uid = this.authService.getActiveUser().uid;
+    return this.http.put('https://ionic3-recipebook-playground.firebaseio.com/' + uid + '/shopping-list.json?auth=' + token, this.ingredients)
+      .map((response : Response) => {
+        return response.json();
+      });
+  }
+
+  fetchList(token: string) {
+    const uid = this.authService.getActiveUser().uid;
+    return this.http.get('https://ionic3-recipebook-playground.firebaseio.com/' + uid + '/shopping-list.json?auth=' + token)
+      .map((response : Response) => {
+        return response.json();
+      }).do(data => {
+        this.ingredients = data;
+      });
+  }
+
 }
